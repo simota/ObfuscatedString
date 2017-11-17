5a39f4a337cddd2740e8a9bf646c889fde0d3460
simota <simota@me.com>
2017-11-18 07:27:23 +0900

    Add

---
+++ ObfuscatedString.cs
@@ -0,0 +1,379 @@
+public static class ObfuscatedString
+{
+    public static string _A(this string value)
+    {
+        return value + "A";
+    }
+    public static string _B(this string value)
+    {
+        return value + "B";
+    }
+    public static string _C(this string value)
+    {
+        return value + "C";
+    }
+    public static string _D(this string value)
+    {
+        return value + "D";
+    }
+    public static string _E(this string value)
+    {
+        return value + "E";
+    }
+    public static string _F(this string value)
+    {
+        return value + "F";
+    }
+    public static string _G(this string value)
+    {
+        return value + "G";
+    }
+    public static string _H(this string value)
+    {
+        return value + "H";
+    }
+    public static string _I(this string value)
+    {
+        return value + "I";
+    }
+    public static string _J(this string value)
+    {
+        return value + "J";
+    }
+    public static string _K(this string value)
+    {
+        return value + "K";
+    }
+    public static string _L(this string value)
+    {
+        return value + "L";
+    }
+    public static string _M(this string value)
+    {
+        return value + "M";
+    }
+    public static string _N(this string value)
+    {
+        return value + "N";
+    }
+    public static string _O(this string value)
+    {
+        return value + "O";
+    }
+    public static string _P(this string value)
+    {
+        return value + "P";
+    }
+    public static string _Q(this string value)
+    {
+        return value + "Q";
+    }
+    public static string _R(this string value)
+    {
+        return value + "R";
+    }
+    public static string _S(this string value)
+    {
+        return value + "S";
+    }
+    public static string _T(this string value)
+    {
+        return value + "T";
+    }
+    public static string _U(this string value)
+    {
+        return value + "U";
+    }
+    public static string _V(this string value)
+    {
+        return value + "V";
+    }
+    public static string _W(this string value)
+    {
+        return value + "W";
+    }
+    public static string _X(this string value)
+    {
+        return value + "X";
+    }
+    public static string _Y(this string value)
+    {
+        return value + "Y";
+    }
+    public static string _Z(this string value)
+    {
+        return value + "Z";
+    }
+    public static string _a(this string value)
+    {
+        return value + "a";
+    }
+    public static string _b(this string value)
+    {
+        return value + "b";
+    }
+    public static string _c(this string value)
+    {
+        return value + "c";
+    }
+    public static string _d(this string value)
+    {
+        return value + "d";
+    }
+    public static string _e(this string value)
+    {
+        return value + "e";
+    }
+    public static string _f(this string value)
+    {
+        return value + "f";
+    }
+    public static string _g(this string value)
+    {
+        return value + "g";
+    }
+    public static string _h(this string value)
+    {
+        return value + "h";
+    }
+    public static string _i(this string value)
+    {
+        return value + "i";
+    }
+    public static string _j(this string value)
+    {
+        return value + "j";
+    }
+    public static string _k(this string value)
+    {
+        return value + "k";
+    }
+    public static string _l(this string value)
+    {
+        return value + "l";
+    }
+    public static string _m(this string value)
+    {
+        return value + "m";
+    }
+    public static string _n(this string value)
+    {
+        return value + "n";
+    }
+    public static string _o(this string value)
+    {
+        return value + "o";
+    }
+    public static string _p(this string value)
+    {
+        return value + "p";
+    }
+    public static string _q(this string value)
+    {
+        return value + "q";
+    }
+    public static string _r(this string value)
+    {
+        return value + "r";
+    }
+    public static string _s(this string value)
+    {
+        return value + "s";
+    }
+    public static string _t(this string value)
+    {
+        return value + "t";
+    }
+    public static string _u(this string value)
+    {
+        return value + "u";
+    }
+    public static string _v(this string value)
+    {
+        return value + "v";
+    }
+    public static string _w(this string value)
+    {
+        return value + "w";
+    }
+    public static string _x(this string value)
+    {
+        return value + "x";
+    }
+    public static string _y(this string value)
+    {
+        return value + "y";
+    }
+    public static string _z(this string value)
+    {
+        return value + "z";
+    }
+    public static string _0(this string value)
+    {
+        return value + "0";
+    }
+    public static string _1(this string value)
+    {
+        return value + "1";
+    }
+    public static string _2(this string value)
+    {
+        return value + "2";
+    }
+    public static string _3(this string value)
+    {
+        return value + "3";
+    }
+    public static string _4(this string value)
+    {
+        return value + "4";
+    }
+    public static string _5(this string value)
+    {
+        return value + "5";
+    }
+    public static string _6(this string value)
+    {
+        return value + "6";
+    }
+    public static string _7(this string value)
+    {
+        return value + "7";
+    }
+    public static string _8(this string value)
+    {
+        return value + "8";
+    }
+    public static string _9(this string value)
+    {
+        return value + "9";
+    }
+    public static string _space(this string value)
+    {
+        return value + " ";
+    }
+    public static string _point(this string value)
+    {
+        return value + ".";
+    }
+    public static string _dash(this string value)
+    {
+        return value + "-";
+    }
+    public static string _comma(this string value)
+    {
+        return value + ",";
+    }
+    public static string _semicolon(this string value)
+    {
+        return value + ";";
+    }
+    public static string _colon(this string value)
+    {
+        return value + ": ";
+    }
+    public static string _apostrophe(this string value)
+    {
+        return value + "'";
+    }
+    public static string _quotation(this string value)
+    {
+        return value + "\"";
+    }
+    public static string _plus(this string value)
+    {
+        return value + "+";
+    }
+    public static string _equals(this string value)
+    {
+        return value + "=";
+    }
+    public static string _paren_left(this string value)
+    {
+        return value + "(";
+    }
+    public static string _paren_right(this string value)
+    {
+        return value + ")";
+    }
+    public static string _asterisk(this string value)
+    {
+        return value + "*";
+    }
+    public static string _ampersand(this string value)
+    {
+        return value + "&";
+    }
+    public static string _caret(this string value)
+    {
+        return value + "^";
+    }
+    public static string _percent(this string value)
+    {
+        return value + "%";
+    }
+    public static string _dollar(this string value)
+    {
+        return value + "$";
+    }
+    public static string _pound(this string value)
+    {
+        return value + "#";
+    }
+    public static string _at(this string value)
+    {
+        return value + "@";
+    }
+    public static string _exclamation(this string value)
+    {
+        return value + "!";
+    }
+    public static string _question_mark(this string value)
+    {
+        return value + "?";
+    }
+    public static string _back_slash(this string value)
+    {
+        return value + "\\";
+    }
+    public static string _forward_slash(this string value)
+    {
+        return value + "/";
+    }
+    public static string _curly_left(this string value)
+    {
+        return value + "{";
+    }
+    public static string _curly_right(this string value)
+    {
+        return value + "}";
+    }
+    public static string _bracket_left(this string value)
+    {
+        return value + "[";
+    }
+    public static string _bracket_right(this string value)
+    {
+        return value + "]";
+    }
+    public static string _bar(this string value)
+    {
+        return value + "|";
+    }
+    public static string _less_than(this string value)
+    {
+        return value + "<";
+    }
+    public static string _greater_than(this string value)
+    {
+        return value + ">";
+    }
+    public static string _underscore(this string value)
+    {
+        return value + "_";
+    }
+    public static string _dot(this string value)
+    {
+        return value + ".";
+    }
+}
